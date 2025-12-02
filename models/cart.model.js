f1d539e27d901538f049f9f2206ede39533e6d06
marygracenicoleevangelista-crypto <marygracenicole.evangelista@neu.edu.ph>
2025-12-02 22:34:35 +0800

    Recovered controllers, middleware, and models; working version

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,13 +1,27 @@
+// models/cart.model.js
 const mongoose = require("mongoose");
 
+const cartItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1
+  }
+}, { _id: false });
+
 const cartSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  items: [
-    {
-      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-      quantity: { type: Number, default: 1, min: 1 }
-    }
-  ]
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true
+  },
+  items: [cartItemSchema]
 }, { timestamps: true });
 
-module.exports = mongoose.models.Cart || mongoose.model("Cart", cartSchema);
+module.exports = mongoose.model("Cart", cartSchema);
