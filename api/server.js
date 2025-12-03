d4b7c0b3b00987b2d2a35517cb3e841cd1300d1c
marygracenicoleevangelista-crypto <marygracenicole.evangelista@neu.edu.ph>
2025-12-03 15:48:40 +0800

    Enable Helmet with Swagger configuration

--- api/server.js
+++ api/server.js
@@ -12,9 +12,18 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false, 
+    contentSecurityPolicy: false, 
+  })
+);
 
 // Routes
 app.use("/api/products", require("../routes/product.routes"));
