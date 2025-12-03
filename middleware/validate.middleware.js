4e0f78002bbaf4ad6453ee0e31cbcf28684b7fea
marygracenicoleevangelista-crypto <marygracenicole.evangelista@neu.edu.ph>
2025-12-03 14:23:22 +0800

    Final save before merge

--- middleware/validate.middleware.js
+++ middleware/validate.middleware.js
@@ -1,9 +1,9 @@
-const { validationResult } = require("express-validator");
+const { validationResult } = require("express-validator"); 
 
-module.exports = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ success: false, message: "Validation failed", data: errors.array() });
-  }
-  next();
-};
+module.exports = (req, res, next) => { 
+    const errors = validationResult(req); 
+    if (!errors.isEmpty()) { 
+        return res.status(400).json({ success: false, message: "Validation failed", 
+            data: errors.array() }); } 
+        next(); 
+    };
